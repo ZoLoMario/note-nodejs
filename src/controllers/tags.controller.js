a58b49035252963afd03999b106fdf4c3f22d9b5
dangkhoi-lap <dangkhoi.kvip@gmail.com>
2020-10-08 16:22:54 +0700

    them taglist

---
+++ src/controllers/tags.controller.js
@@ -0,0 +1,40 @@
+const tagsCtrl = {};
+
+// Models
+const Tag = require("../models/Tag");
+
+tagsCtrl.createTag = async (req, res) => {
+  const tagadd = req.body.tagadd;
+  console.log("create tag : " + tagadd);
+  const errors = [];
+  if (!tagadd) {
+    errors.push({ text: "Please Write a Tag." });
+  };
+  if (errors.length > 0) {
+    res.send( {
+      errors,
+      tagadd
+    });
+  } else {
+    const newTag = new Tag(tagadd);
+    newTag.user = req.user.id;
+    await newTag.save();
+    req.flash("success_msg", "Tag Added Successfully");
+    res.send("Thêm Tag thành công");
+  }
+};
+
+tagsCtrl.renderTags = async (req, res) => {
+  const tags = await Note.find({ user: req.user.id })
+    .sort({ date: "desc" })
+    .lean();
+  res.send(tags);
+};
+
+tagsCtrl.deleteTag = async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Tag Deleted Successfully");
+  res.send("Tag Deleted Successfully");
+};
+
+module.exports = tagsCtrl;
