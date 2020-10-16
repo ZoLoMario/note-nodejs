0011054929278db17e5de2e751ec794c7a5f13ea
dangkhoi <dangkhoi.kvip@gmail.com>
2020-10-16 07:13:14 +0700

    bat dau lien ket note

--- src/models/Tag.js
+++ src/models/Tag.js
@@ -1,11 +1,13 @@
 const { Schema, model } = require("mongoose");
+const Note = require("./Note");
 
 const TagSchema = new Schema(
   {
     tag: {
       type: String,
       required: true
-    }
+    },
+	tag: [{ type: Schema.Types.ObjectId, ref: 'Note' }]
   },
   {
     timestamps: true
